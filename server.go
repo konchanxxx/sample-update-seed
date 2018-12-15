fdb22dd93b90925828b770f45bdca7a7eb0d473c
rexitorg <rexit.org@gmail.com>
2018-12-15 21:37:22 +0900

    set defined environment port

--- server.go
+++ server.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/rexitorg/sample-update-seed/router"
 )
 
 func main() {
 	r := router.NewRouter()
-	err := http.ListenAndServe(":8080", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Set default port: %s", port)
+	}
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 	if err != nil {
 		log.Fatalf("Failed http listen and serve: %#v", err)
 	}
